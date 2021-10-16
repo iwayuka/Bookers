f7223897c6c0dba8d37a293ef4b2e61ed1e4bcd0
iwayuka <ec2-user@ip-172-31-92-11.ec2.internal>
2021-10-16 09:03:40 +0000

    [Add] New And Create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,24 @@ class BooksController < ApplicationController
   def index
   end
 
-  def show
+  def new
+    @book = Book.new
   end
 
-  def new
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path
+  end
+
+  def show
   end
 
   def edit
   end
+
+  def destory
+  end
+
+
 end
