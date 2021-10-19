ffb30a7fc75c2f5cab7967e8ce050cd12a982342
iwayuka <iwabuchi.yuka07@gmail.com>
2021-10-19 10:19:48 +0000

    [Fix]
    Validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,15 +24,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       redirect_to book_path(book.id), notice: 'Book was successfully updated.'
     else
-      render 'books/edit'
+      render :edit
     end
   end
-
-
 
   def destroy
     @book = Book.find(params[:id])
